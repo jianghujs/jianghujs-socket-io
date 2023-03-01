24c8f4ddb66bf8eeef3d7e6ecbb3bc2d78a0fa51
yuebenshan <yuebenshan@users.noreply.github.com>
2023-03-01 21:41:46 +0800

    jianghujs 4.0.25

--- config/plugin.js
+++ config/plugin.js
@@ -1,5 +1,12 @@
 'use strict';
+const path = require('path');
+const eggJianghuDirResolve = require.resolve('@jianghujs/jianghu');
+const eggJianghuDir = path.join(eggJianghuDirResolve, '../');
 
 /** @type Egg.EggPlugin */
 module.exports = {
+  socketIO: {
+    enable: true,
+    path: path.join(eggJianghuDir, 'plugins/socket-io')
+  }
 };
